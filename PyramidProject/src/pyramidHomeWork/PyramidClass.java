07933d990941652cef179fd5f96348f2348d01cd
djafar1981 <49218986+djafar1981@users.noreply.github.com>
2019-04-23 14:30:58 -0400

    Add files via upload

---
+++ PyramidProject/src/pyramidHomeWork/PyramidClass.java
@@ -0,0 +1,23 @@
+package pyramidHomeWork;
+
+public class PyramidClass {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<10; i++){
+
+            for(int x =9; x>=i; x--){
+                System.out.print(" ");
+            }
+            for (int z=0; z<=i; z++){
+                System.out.print(" *");
+            }
+
+            System.out.println(" ");
+
+        }
+    }
+
+
+
+}
